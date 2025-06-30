34c7ed79214baec9eafd86913e1e0b40655fc279
KINGBK1 <bishalkunwar140405@gmail.com>
2025-06-30 14:49:48 +0530

    backend vulnerabilites removed -> 2

--- server/routes/documentRoutes.js
+++ server/routes/documentRoutes.js
@@ -19,7 +19,7 @@ const router = express.Router();
 
 router.post("/", authMiddleware, createDocument);
 router.get("/my-docs", authMiddleware, getMyDocuments);
-router.get("/:id", authMiddleware,checkDocumentAccess, getDocumentById);
+router.get("/:id", authMiddleware, getDocumentById);
 router.delete("/:id", authMiddleware, deleteMyDoc);
 router.patch("/:id/access", authMiddleware, toggleAccess);
 router.post("/:id/request-access", authMiddleware, requestAccess);
